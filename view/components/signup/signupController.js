5354aab8edf82c9319405a2bdc435cb93aa14af6
lapinoux02 <paulbnt@free.fr>
2016-03-21 22:25:51 +0100

    Add language taglist

--- view/components/signup/signupController.js
+++ view/components/signup/signupController.js
@@ -1,20 +1,31 @@
 angular.module("heinModule")
-.controller("signupController", function($scope, $state, $stateParams, signupService){
+.controller("signupController", function ($scope, $state, $stateParams, signupService) {
     if (parseInt($stateParams.stepId) < 1 || parseInt($stateParams.stepId) > 4) {
         $state.go(signup, {stepId: 1});
     }
     
     $scope.currentStep = $stateParams.stepId;
-
     $scope.user = signupService.getUser();
     
-    $scope.submit = function() {
+    $scope.removeTag = function (index) {
+        $scope.user.spokenLanguages.splice(index, 1);
+    };
+    
+    $scope.submit = function () {
+        if ($scope.currentStep == 2) {
+            $scope.user.spokenLanguages.push(angular.copy($scope.user.newLanguage));
+            $scope.user.newLanguage = "";
+            return;
+        }
         $state.go($state.current, {stepId: parseInt($stateParams.stepId) + 1});
     };
     
-    $scope.back = function() {
-        $scope.user.spokenLanguages.slice(0, $scope.user.spokenLanguages.length) ;
-        $state.go($state.current, {stepId: parseInt($stateParams.stepId) - 1});
+    $scope.goToStep = function (targetStep) {
+        if (targetStep <= 1 && $scope.currentStep > 1) {
+            $scope.user.spokenLanguages.slice(0, $scope.user.spokenLanguages.length) ;
+        }
+        
+        $state.go($state.current, {stepId: targetStep});
     }
 
 });

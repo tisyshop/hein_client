f74cff76c7f1e59bf29d5b5794fe457ede5e9628
lapinoux02 <paulbnt@free.fr>
2016-03-25 23:42:01 +0100

    Comments and code structure for the signup page

--- view/components/signup/signupController.js
+++ view/components/signup/signupController.js
@@ -1,28 +1,44 @@
 angular.module("heinModule")
 .controller("signupController", function ($scope, $state, $stateParams, signupService) {
+    
+    // Check the step given
     if (!signupService.checkStep(parseInt($stateParams.stepId))) {
-        $state.go('signup', {stepId: 1});
+        // If not, go to step 1
+        $state.go("signup", {stepId: 1});
     };
     
+    // Set the controller state's parameters
     $scope.currentStep = $stateParams.stepId;
     $scope.user = signupService.getUser();
     
+    /*
+     * Step 1 methods
+     */
+    $scope.submitStep1 = function() {
+        $scope.goToStep(2);
+    }
+    
+    /*
+     * Step 2 methods
+     */
+    
+    // Remove a language tag
     $scope.removeTag = function (index) {
         $scope.user.spokenLanguages.splice(index, 1);
     };
     
-    $scope.submit = function () {
-        if ($scope.currentStep == 2) {
-            if (!$scope.user.newLanguage) {
-                return;
-            }
-            $scope.user.spokenLanguages.push(angular.copy($scope.user.newLanguage));
-            $scope.user.newLanguage = "";
+    $scope.submitStep2 = function() {
+        if (!$scope.user.newLanguage) {
             return;
         }
-        $state.go($state.current, {stepId: parseInt($stateParams.stepId) + 1});
+        $scope.user.spokenLanguages.push(angular.copy($scope.user.newLanguage));
+        $scope.user.newLanguage = "";
+        return;
     };
     
+    /*
+     * Global methods
+     */
     $scope.goToStep = function (targetStep) {
         if (targetStep <= 1 && $scope.currentStep > 1) {
             $scope.user.spokenLanguages.slice(0, $scope.user.spokenLanguages.length) ;
